2079b26266d7239f64b2947a6f48a95a95df8943
Minesims <minesims@live.fr>
2026-01-19 15:55:40 +0100

    Fix: Second batch of JS code (in progress)

--- script.js
+++ script.js
@@ -16,68 +16,52 @@ let input = "";
 
 searchInput.addEventListener('keyup', ()=> {
   input = searchInput.value;
-  if (input != "") {
+  if (input.length > 0) {
     input = input[0].toUpperCase() + input.slice(1);
     filterCities();
     console.log(input);
   } else {
-    input = "";
+    cityList.innerHTML = "";
   }
 });
-
 
 // TODO : filtrer les villes selon la saisie
 // TODO : afficher la liste des villes correspondantes
 // TODO : gérer le clic sur une ville
+// TODO : gestion de la classe CSS active
 
 function filterCities() {
   cities.forEach(city => {
     if (city.name.includes(input)) {
       // cityList.innerHTML = ('');
-      cityList.innerHTML = (`<li class="city">${city.name}</li>`);
+      cityList.innerHTML += (`<li class="city">${city.name}</li>`);
     } 
 });
+
 
   let citiesChoice = document.querySelectorAll('.city');
   
   citiesChoice.forEach(city => {
     city.addEventListener('click', () => {
+      fetch('https://api.open-meteo.com/v1/forecast?latitude=48.8534,45.7485,43.297,44.8404,50.633&longitude=2.3488,4.8467,5.3811,-0.5805,3.0586&current=temperature_2m')
+      .then(response => response.json())
+      .then(data => {
+        weatherDiv.innerHTML = `<p>${cities[d].name} : ${data[d].current.temperature_2m} °C</p>`;
+      })
+      .catch(error => {
+        return error;
+      });
+
       city.classList.add('active');
-      showMeteo();
+
+      });
     })
-  });
-};
+
+  };
 
 // TODO : requête AJAX vers Open-Meteo
-
-function fetchMeteo() {
-  fetch('https://api.open-meteo.com/v1/forecast?latitude=48.8534,45.7485,43.297,44.8404,50.633&longitude=2.3488,4.8467,5.3811,-0.5805,3.0586&current=temperature_2m')
-  .then(response => response.json)
-  .then(data => {
-    return data;
-  })
-  .catch(error => {
-      return error;
-  });
-};
 
 
 // TODO : affichage météo dans le DOM
 
-let meteoData = '';
-
-function showMeteo() {
-  fetchMeteo();
-  meteoData = fetchMeteo;
-  data.forEach(data => {
-    for (let d = 0; d < cities.length; d++) {
-      if (d == data.location_id) {
-        weather.innerHTML = (`${cities[d].name} : ${temperature_2m} °C`)
-      }
-    }
-  });
-}
-
-console.log(meteoData)
-
-// TODO : gestion de la classe CSS active
+// console.log(meteoData);
